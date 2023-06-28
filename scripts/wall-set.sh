e01673d6c769bbc6820699535ce20138a49105d2
Adam <shreyas.as.tech@protonmail.com>
2023-06-28 15:13:03 +0530

    Replacing comparison string in wall-set.sg (nireogen -> --set-zoom-fill. Coz otherwise it would replace the floating window properties also)

--- scripts/wall-set.sh
+++ scripts/wall-set.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "/nitrogen/ c\nitrogen --restore &" ~/.config/qtile/autostart.sh
+sed -i "/--set-zoom-fill/ c\nitrogen --restore &" ~/.config/qtile/autostart.sh
 sed -i "/wall/ c\\" ~/.config/qtile/config.py
 rm ~/wall-set.sh
